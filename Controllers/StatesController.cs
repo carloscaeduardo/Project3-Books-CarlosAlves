de5ca6229438248d1e4ebab28ae5433dd7a2267f
Carlos Alves [Kuro Suzume] <carlos.ca.eduardo@gmail.com>
2022-06-05 22:01:16 -0300

    Add project files.

---
+++ Controllers/StatesController.cs
@@ -0,0 +1,139 @@
+﻿using Project3_Books_CarlosAlves.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project3_Books_CarlosAlves.Controllers
+{
+    public class StatesController : Controller
+    {
+        // GET: States
+        public ActionResult AllStates(string id, int sortBy = 0, bool isDesc = false)
+        {
+            var context = new BooksEntities();
+            List<State> states = context.States.ToList();
+
+            switch (sortBy)
+            {
+           
+                case 1:
+                    {
+                        if (isDesc)
+                        {
+                            states = context.States.OrderByDescending(s => s.StateName).ToList();
+                        }
+                        else
+                        {
+                            states = context.States.OrderBy(s => s.StateName).ToList();
+                        }
+
+                        break;
+                    }
+            
+                case 0:
+                default:
+                    {
+                        if (isDesc)
+                        {
+                            states = context.States.OrderByDescending(s => s.StateCode).ToList();
+                        }
+                        else
+                        {
+                            states = context.States.OrderBy(s => s.StateCode).ToList();
+                        }
+
+
+                        break;
+                    }
+                   
+
+            }
+
+
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                id = id.Trim().ToLower();
+
+                states = states.Where(s =>
+                                       s.StateCode.ToLower().Contains(id) ||
+                                       s.StateName.ToLower().Contains(id)).ToList();
+
+
+            }
+
+            return View(states);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult UpsertState(string id)
+        {
+            BooksEntities context = new BooksEntities();
+            var stateToSave = context.States.Where(s => s.StateCode == id).FirstOrDefault();
+            if (stateToSave == null)
+            {
+                stateToSave = new State();
+            }
+            return View(stateToSave);
+        }
+
+        [HttpPost]
+        public ActionResult UpsertState(State newState)
+        {
+            BooksEntities context = new BooksEntities();
+
+            try
+            {
+                if (context.States.Where(s => s.StateCode == newState.StateCode).Count() > 0)
+                {
+                    var stateToSave = context.States.Where(s => s.StateCode == newState.StateCode).FirstOrDefault();
+
+                    stateToSave.StateName = newState.StateName;
+    
+
+                }
+                else
+                {
+                    context.States.Add(newState);
+                }
+                context.SaveChanges();
+
+
+
+
+            }
+            catch (System.Exception)
+            {
+                throw;
+                // log the exception in the error log, and send an automatic email to IT Support
+                //return RedirectToAction("Error");
+            }
+            return RedirectToAction("AllStates");
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(string id)
+        {
+            BooksEntities context = new BooksEntities();
+            string stateCode = id;
+          
+            try
+            {
+                State state = context.States.Where(s => s.StateCode == stateCode).FirstOrDefault();
+                context.States.Remove(state);
+
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+   
+            return RedirectToAction("AllStates");
+        }
+    }
+}
---
+++ Scripts/main.js
@@ -0,0 +1,76 @@
+﻿var Sortable = {
+    baseUrl: '',
+    sortBy: 0,
+    searchTerm: '',
+    Search() {
+        var searchKey = $('#txtSearch').val();
+        window.location.href = Sortable.baseUrl + "?id=" + searchKey;
+
+    },
+    Sort(sortBy) {
+        var isDesc = true;
+
+        // how to detect from the url if isDesc is true or false
+        const urlParams = new URLSearchParams(window.location.search);
+
+        var isDescOriginal = urlParams.get('isDesc');
+        const sortByOriginal = urlParams.get('sortBy');
+
+        // sorting the same column, means we need to change the isDesc value
+        if (sortByOriginal == sortBy) {
+            if (isDescOriginal == 'true') {
+                isDesc = false;
+            }
+
+        }
+
+
+
+        window.location.href = Sortable.baseUrl + "?sortBy=" + sortBy + "&isDesc=" + isDesc;
+
+    }
+};
+
+
+
+var apiHandler = {
+    GET(url) {
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function (res) {
+                debugger;
+            }
+        });
+    },
+    POST(url, object) {
+        object = {
+            Id: 5,
+            Name: "asd",
+            Email: "test@gmail.com",
+        }
+        $.ajax({
+            url: url,
+            type: 'GET',
+            data: object,
+            success: function (res) {
+                debugger;
+            }
+        });
+    },
+    DELETE(url) {
+        if (confirm("Are you sure you want to delete ?")) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (res) {
+                    debugger;
+                }
+            });
+        } else {
+            alert("Ok, delete cancelled.");
+        }
+
+    }
+};
+
